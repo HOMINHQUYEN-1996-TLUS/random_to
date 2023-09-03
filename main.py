1b80b21445af7c7cca71dea753cb89dc235197f1
Quyen Ho <46450315+HOMINHQUYEN-1996-TLUS@users.noreply.github.com>
2023-09-03 10:46:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,3 @@
-# import openpyxl
-
-# # Mở tệp Excel chứa 50 tên
-# workbook = openpyxl.load_workbook('Book1.xlsx')
-# sheet_goc = workbook.active
-
-# # Tạo 5 sheet mới
-# for i in range(1, 6):
-#     new_sheet = workbook.create_sheet(title=f'Sheet{i}')
-
-# # Tạo danh sách tên không lặp lại
-# ten_khong_lap_lai = list(set(sheet_goc['A1:A50']))
-
-# # Phân chia danh sách tên vào 5 phần bằng số phần tử gần bằng nhau
-# so_phan_tu_moi_sheet = len(ten_khong_lap_lai) // 5
-
-# # Bắt đầu phân chia tên vào các sheet
-# for i, sheet in enumerate(workbook.sheetnames[1:], start=1):
-#     current_sheet = workbook[sheet]
-#     for j in range(so_phan_tu_moi_sheet):
-#         # Chuyển tuple thành giá trị cụ thể (value)
-#         ten = ten_khong_lap_lai.pop()[0].value
-#         current_sheet.cell(row=j + 1, column=1, value=ten)
-
-# # Lưu tệp Excel sau khi đã chỉnh sửa
-# workbook.save('tên_mới.xlsx')
 
 import openpyxl
 
